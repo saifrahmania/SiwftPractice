76dc61cad0735e518bea88cf4c0e97caaec7bf14
saifrahmania <89687720+saifrahmania@users.noreply.github.com>
2022-11-24 08:02:36 +0600

    Create TypeCasting.swift

---
+++ TypeCasting.swift
@@ -0,0 +1,36 @@
+// MARK: Type Casting
+class MediaItem{
+    var name:String
+    init(name:String){
+        self.name = name
+        
+    }
+}
+
+class Movie:MediaItem{
+    var director:String
+    init(name:String,director:String){
+        self.director = director
+        super.init(name: name)
+    }
+}
+
+class Song:MediaItem{
+    var artist:String
+    init(name:String,artist:String){
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [Movie(name: "Casa", director: "Blanka"),
+Song(name: "Blue", artist: "Suede")]
+
+for i in library{
+    if let m = i as? Movie{
+        print(m.director)
+    }
+    else if i is Song{
+        print((i as! Song).artist)
+    }
+}
